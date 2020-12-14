ac22093f26c1c5c477278e40d0d70b510f9e5a9e
developjik@gmail.com <developjik@gmail.com>
2020-12-14 19:56:24 +0900

    deploy with face object record

--- src/components/views/RoomPage/JoinRoomPage.js
+++ src/components/views/RoomPage/JoinRoomPage.js
@@ -238,8 +238,8 @@ function JoinRoomPage(props) {
 
       startFaceApi();
       startObjectDetect();
-      startMyModel();
-      startRecognizeFaces();
+      // startMyModel();
+      // startRecognizeFaces();
       
     } else {
       state.session
@@ -263,8 +263,8 @@ function JoinRoomPage(props) {
     document.querySelector("#startTest").disabled = false;
     clearInterval(faceapiInterval);
     clearInterval(objectdetectInterval);
-    clearInterval(sidefaceInterval);
-    clearInterval(faceReocogCheckInterval())
+    // clearInterval(sidefaceInterval);
+    // clearInterval(faceReocogCheckInterval())
     await records.stop();
     blob = records.getBlob();
     uploadVideo();
